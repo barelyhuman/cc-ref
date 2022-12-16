97649bbd838e7d0b318d6143d55a67ed74ee10a1
reaper <ahoy@barelyhuman.dev>
2022-12-16 11:20:15 +0530

    hover state addition

--- src/components/pressable.jsx
+++ src/components/pressable.jsx
@@ -1,7 +1,7 @@
 export function PressableItem({ href, title }) {
   return (
-    <a className="active:bg-zinc-800" href={href}>
-      <div className="border-b border-zinc-800 px-4 py-6">
+    <a className="" href={href}>
+      <div className="active:bg-zinc-800 hover:bg-zinc-800 hover:pointer border-b border-zinc-800 px-4 py-6">
         <p className="font-title text-lg text-zinc-400">{title}</p>
       </div>
     </a>
